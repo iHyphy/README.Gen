93c8e5e2883845b932bf6d65577950dd7693d1ff
Manraj Uppal <uppalmanraj@gmail.com>
2024-03-18 10:27:29 -0700

    Updated Index.js

--- index.js
+++ index.js
@@ -1,13 +1,96 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs'); 
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        
+    }, 
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter the description of your project:', 
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    }, 
+    {
+        type: 'input',
+        name: 'installations',
+        message: 'What are the installation instructions?', 
+        
+    }, 
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the intended function of your project?', 
+        
+    }, 
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license are you using for this project?', 
+        choices: [ 
+                    'MIT License', 
+                    'Apache License 2.0',     
+                    'Eclipse Public License',
+                    'no license'
+                ],
+        default: 'no license'
+    }, 
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can one wish to contribute?', 
+        
+    }, 
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions?',
+    
+    }, 
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What email would you like to provide for questions or concerns? (if there are any)', 
+        
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your github username?',
 
-// TODO: Create a function to initialize app
-function init() {}
+    }
+];
+
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(`${fileName} has been created successfully!`);
+    });
+}
+
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then(answers => {
+            // Generate markdown content
+            const markdown = generateMarkdown(answers);
+            // Write markdown content to README.md file
+            writeToFile('READMEGEN.md', markdown);
+        })
+        .catch(error => {
+            console.error('An error occurred while prompting questions:', error);
+        });
+}
 
 // Function call to initialize app
 init();
